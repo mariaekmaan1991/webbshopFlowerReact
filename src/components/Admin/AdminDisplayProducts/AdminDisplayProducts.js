8a5abfe0be1fd94ebdbaff9e6481b7ce21fa2eab
mariaekmaan1991 <mariaekmaan@hotmail.com>
2021-01-06 13:47:56 +0100

    style productlist

--- src/components/Admin/AdminDisplayProducts/AdminDisplayProducts.js
+++ src/components/Admin/AdminDisplayProducts/AdminDisplayProducts.js
@@ -17,25 +17,50 @@ export function AdminDisplayProducts({
     ListProduct.map((data, id) => {
       console.log(data, "hh");
       return (
-        <>
-          <h1 key={id}>{data.name}</h1>
-          <h1>{data.price}</h1>
-          <div>{`name: ${data.name}`}</div>
-          <img src={data.imageUrl} alt="" />
-          <div>{`price: ${data.price}`}</div>
-          <div>{`description: ${data.description}`}</div>
-          <div>{`id: ${data.id}`}</div>
-          <div>{` category: ${data.category}`}</div>
-          <button onClick={() => deleteProduct(data.id)}>radera</button>
-          <button onClick={() => setUpdateFormValues(data)}>upptatera</button>
-        </>
+        <div className="Admin-Display-Product-Main-Container">
+          <div className="Admin-Display-Product-Container" key={id}>
+            <div className="Admin-Display-Product-Container-Name">
+              {`id: ${data.id}`}
+              <h2 className="Admin-Display-Product-Name">
+                {`Titel: ${data.name}`}
+              </h2>
+            </div>
+            <div className="Admin-Display-Product-Container-ImageUrl">
+              <img
+                className="Admin-Display-Product-ImageUrl"
+                src={data.imageUrl}
+                alt=""
+              />
+            </div>
+            <div className="Admin-Display-Product-Container-Info">
+              <h3> {`price: ${data.price}`}</h3>
+              <p>{`description: ${data.description}`}</p>
+            </div>
+            <div className="Admin-Display-Product-Container-Category">
+              <p>{` category: ${data.category}`}</p>
+            </div>
+            <div className="Admin-Display-Product-Container-Category">
+              <button
+                className="Admin-Display-Product-Container-Button"
+                onClick={() => deleteProduct(data.id)}
+              >
+                radera
+              </button>
+              <button
+                className="Admin-Display-Product-Container-Button"
+                onClick={() => setUpdateFormValues(data)}
+              >
+                upptatera
+              </button>
+            </div>
+          </div>
+        </div>
       );
     });
 
   return (
-    <div>
-      <h1>hej</h1>
+    <React.Fragment>
       <div>{productList}</div>
-    </div>
+    </React.Fragment>
   );
 }
